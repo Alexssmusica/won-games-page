09a6bd10bf52cd8763209cd6ff6de9240a830543
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-03 22:49:55 -0300

    Criado uma outra prop da logo usando TDD

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
-import Buttom from '.';
+import Logo from '.';
 
-describe('<Buttom />', () => {
-    it('should render the heading', () => {
-        const { container } = render(<Buttom />);
+describe('<Logo />', () => {
+    it('should render a white label by default', () => {
+        renderWithTheme(<Logo />);
+        expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+            color: '#FAFAFA'
+        });
+    });
 
-        expect(screen.getByRole('heading', { name: /Buttom/i })).toBeInTheDocument();
+    it('should render a black label when color is passed', () => {
+        renderWithTheme(<Logo color="black" />);
+        expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+            color: '#030517'
+        });
+    });
 
-        expect(container.firstChild).toMatchSnapshot();
+    it('should render a normal logo when size is default', () => {
+        renderWithTheme(<Logo />);
+        expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+            width: '11rem'
+        });
+    });
+
+    it('should render a bigger logo', () => {
+        renderWithTheme(<Logo size="large" />);
+        expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+            width: '20rem'
+        });
     });
 });
