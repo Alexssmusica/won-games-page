430844b3eb6be3447324366d24797925978d8f27
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-11 21:04:51 -0300

    Add Ribbon no Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,5 @@
-import Buttom from '../Buttom';
+import Buttom from 'components/Buttom';
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon';
 import * as Style from './styles';
 
 export type BannerProps = {
@@ -7,10 +8,28 @@ export type BannerProps = {
     subtitle: string;
     buttonLabel: string;
     buttonLink: string;
+    ribbon?: string;
+    ribbonSize?: RibbonSizes;
+    ribbonColor?: RibbonColors;
 };
 
-const Banner = ({ img, title, subtitle, buttonLabel, buttonLink }: BannerProps) => (
+const Banner = ({
+    img,
+    title,
+    subtitle,
+    buttonLabel,
+    buttonLink,
+    ribbon,
+    ribbonSize = 'normal',
+    ribbonColor = 'primary'
+}: BannerProps) => (
     <Style.Wrapper>
+        {!!ribbon && (
+            <Ribbon color={ribbonColor} size={ribbonSize}>
+                {ribbon}
+            </Ribbon>
+        )}
+
         <Style.Image src={img} role="img" aria-label={title} />
 
         <Style.Caption>
