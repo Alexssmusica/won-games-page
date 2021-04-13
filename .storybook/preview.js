036290bd141a521300dabc7ec359a931ad827859
Alex Santos de Souza <alexssmusica@gmail.com>
2021-04-13 16:05:46 -0300

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,12 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
+// import * as nextImage from "next/image"
+
+// Object.defineProperty(nextImage, "default", {
+//   configurable: true,
+//   value: props => <img {...props} />
+// })
 
 export const decorators = [
   (Story) => (
