422f2553c9f49edce2db61fd87e1eba2d8ef2dc4
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-17 13:40:14 -0300

    Ajustes GameCard

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,8 +1,61 @@
+import { AddShoppingCart, FavoriteBorder, Favorite } from '@styled-icons/material-outlined';
+
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon';
+import Buttom from 'components/Buttom';
 import * as Style from './styles';
 
-const GameCard = () => (
+export type GameCardProps = {
+    title: string;
+    developer: string;
+    img: string;
+    price: string;
+    promotionalPrice?: string;
+    favorite?: boolean;
+    ribbon?: React.ReactNode;
+    ribbonColor?: RibbonColors;
+    ribbonSize?: RibbonSizes;
+    onFav?: () => void;
+};
+
+const GameCard = ({
+    title,
+    developer,
+    img,
+    price,
+    promotionalPrice,
+    favorite = false,
+    ribbon,
+    ribbonColor = 'primary',
+    ribbonSize = 'small',
+    onFav
+}: GameCardProps) => (
     <Style.Wrapper>
-        <h1>GameCard</h1>
+        {!!ribbon && (
+            <Ribbon color={ribbonColor} size={ribbonSize}>
+                {ribbon}
+            </Ribbon>
+        )}
+        <Style.ImageBox>
+            <img src={img} alt={title} />
+        </Style.ImageBox>
+        <Style.Content>
+            <Style.Info>
+                <Style.Title>{title}</Style.Title>
+                <Style.Developer>{developer}</Style.Developer>
+            </Style.Info>
+            <Style.FavButton onClick={onFav} role="button">
+                {favorite ? (
+                    <Favorite aria-label="Remove from Wishlist" />
+                ) : (
+                    <FavoriteBorder aria-label="Add to Wishlist" />
+                )}
+            </Style.FavButton>
+            <Style.BuyBox>
+                {!!promotionalPrice && <Style.Price isPromotional>{price}</Style.Price>}
+                <Style.Price>{promotionalPrice || price}</Style.Price>
+                <Buttom icon={<AddShoppingCart />} size="small" />
+            </Style.BuyBox>
+        </Style.Content>
     </Style.Wrapper>
 );
 
