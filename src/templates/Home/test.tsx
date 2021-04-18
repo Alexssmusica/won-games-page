db8ca8cbb43eaef83e326291dc25e7eea7a2eda1
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-18 20:11:00 -0300

    Ajustes testes

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -8,13 +8,13 @@ import Home from '.';
 
 const props = {
     banners: bannerMock,
-    newGames: gamesMock,
+    newGames: [gamesMock[0]],
     mostPopularHighlight: highlightMock,
-    mostPopularGames: gamesMock,
-    upcomingGames: gamesMock,
+    mostPopularGames: [gamesMock[0]],
+    upcomingGames: [gamesMock[0]],
     upcomingHighlight: highlightMock,
-    upcomingMoreGames: gamesMock,
-    freeGames: gamesMock,
+    upcomingMoreGames: [gamesMock[0]],
+    freeGames: [gamesMock[0]],
     freeHighlight: highlightMock
 };
 
@@ -25,22 +25,16 @@ describe('<Home />', () => {
         expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
         expect(screen.getByRole('heading', { name: /follow us/i })).toBeInTheDocument();
         expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
-    });
 
-    it('should render sections', () => {
-        renderWithTheme(<Home {...props} />);
         expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument();
         expect(screen.getByRole('heading', { name: /most popular/i })).toBeInTheDocument();
         expect(screen.getByRole('heading', { name: /upcoming/i })).toBeInTheDocument();
         expect(screen.getByRole('heading', { name: /free games/i })).toBeInTheDocument();
-    });
 
-    it('should render section elements', () => {
-        renderWithTheme(<Home {...props} />);
         // banner
         expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1);
-        // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-        expect(screen.getAllByText(/population zero/i)).toHaveLength(20);
+        // card game ( 5 sections com 1 cards cada = 5x1 = 5)
+        expect(screen.getAllByText(/population zero/i)).toHaveLength(5);
         // highlight
         expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3);
     });
