a64c640bda2a42dc53fd5707a1e4aac60903bddf
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-05-22 14:31:42 -0300

    Ajsutes Warning Default PostCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,15 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials',
+     '@storybook/addon-postcss',
+     {
+       name: '@storybook/addon-postcss',
+       options: {
+         postcssLoaderOptions: {
+           implementation: require('postcss'),
+         },
+       },
+    }],
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
