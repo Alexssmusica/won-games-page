28416544b0cb7dac67defaaf9e1e008f80017b0d
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-05-22 16:51:08 -0300

    add icone no TextField

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -7,9 +7,10 @@ export type TextFieldProps = {
     label?: string;
     labelFor?: string;
     initialValue?: string;
+    icon?: React.ReactNode;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-const TextField = ({ label, labelFor = '', initialValue = '', onInput, ...props }: TextFieldProps) => {
+const TextField = ({ icon, label, labelFor = '', initialValue = '', onInput, ...props }: TextFieldProps) => {
     const [value, setValue] = useState(initialValue);
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,6 +24,7 @@ const TextField = ({ label, labelFor = '', initialValue = '', onInput, ...props 
         <Style.Wrapper>
             {!!label && <Style.Label htmlFor={labelFor}>{label}</Style.Label>}
             <Style.InputWrapper>
+                {!!icon && <Style.Icon>{icon}</Style.Icon>}
                 <Style.Input type="text" onChange={onChange} value={value} {...props} />
             </Style.InputWrapper>
         </Style.Wrapper>
