059809458ba176c56cd7ac5befe434ff160b7ef6
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-05-23 11:49:10 -0300

    Add Auth template tests

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,8 +1,33 @@
+import Heading from '../../components/Heading';
+import Logo from '../../components/Logo';
 import * as Style from './styles';
 
-const Auth = () => (
+type AuthProps = {
+    title: string;
+    children: React.ReactNode;
+};
+
+const Auth = ({ title, children }: AuthProps) => (
     <Style.Wrapper>
-        <h1>Auth</h1>
+        <Style.BannerBlock>
+            <Logo />
+
+            <Heading>All your favorite games in one place</Heading>
+            <Style.Subtitle>
+                <strong>WON</strong> is the best and most complete gaming platform.
+            </Style.Subtitle>
+
+            <Style.Footer>Won Games 2020 © Todos os Direitos Reservados</Style.Footer>
+        </Style.BannerBlock>
+
+        <Style.Content>
+            <Logo color="black" size="large" />
+            <Heading color="black" lineColor="secondary" lineLeft>
+                {title}
+            </Heading>
+
+            {children}
+        </Style.Content>
     </Style.Wrapper>
 );
 
