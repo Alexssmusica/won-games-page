0ab088bb83503aaa78a4502dafff24756ebfeb93
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-05-30 19:06:55 -0300

    Add html and tests to GameInfo

---
+++ src/components/GameInfo/index.tsx
@@ -0,0 +1,36 @@
+import { AddShoppingCart, FavoriteBorder } from '@styled-icons/material-outlined';
+
+import Button from '../Buttom';
+import Heading from '../Heading';
+import Ribbon from '../Ribbon';
+
+import * as Style from './styles';
+
+export type GameInfoProps = {
+    title: string;
+    description: string;
+    price: string;
+};
+
+const GameInfo = ({ title, description, price }: GameInfoProps) => (
+    <Style.Wrapper>
+        <Heading color="black" lineBottom>
+            {title}
+        </Heading>
+
+        <Ribbon color="secondary">{`$${price}`}</Ribbon>
+
+        <Style.Description>{description}</Style.Description>
+
+        <Style.ButtonsWrapper>
+            <Button icon={<AddShoppingCart />} size="large">
+                Add to cart
+            </Button>
+            <Button icon={<FavoriteBorder />} size="large" minimal>
+                Wishlist
+            </Button>
+        </Style.ButtonsWrapper>
+    </Style.Wrapper>
+);
+
+export default GameInfo;
