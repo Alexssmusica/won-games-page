254eaad363b0c257c42fd3e7b013f7fff25c6b0d
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-05-30 21:32:00 -0300

    Add GameDetails

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,79 @@
+import { Apple, Windows, Linux } from '@styled-icons/fa-brands';
+
+import Heading from '../Heading';
+import MediaMatch from '../MediaMatch';
+import * as Style from './styles';
+
+type Platform = 'windows' | 'linux' | 'mac';
+
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18';
+
+export type GameDetailsProps = {
+    developer: string;
+    platforms: Platform[];
+    releaseDate: string;
+    rating: Rating;
+    genres: string[];
+};
+
+const GameDetails = ({ developer, releaseDate, platforms, rating, genres }: GameDetailsProps) => {
+    const platformIcons = {
+        linux: <Linux title="Linux" size={18} />,
+        mac: <Apple title="Mac" size={18} />,
+        windows: <Windows title="Windows" size={18} />
+    };
+
+    return (
+        <Style.Wrapper>
+            <MediaMatch greaterThan="small">
+                <Heading lineLeft lineColor="secondary">
+                    Game Details
+                </Heading>
+            </MediaMatch>
+
+            <Style.Content>
+                <Style.Block>
+                    <Style.Label>Developer</Style.Label>
+                    <Style.Description>{developer}</Style.Description>
+                </Style.Block>
+
+                <Style.Block>
+                    <Style.Label>Release Date</Style.Label>
+                    <Style.Description>
+                        {new Intl.DateTimeFormat('en-US', {
+                            day: 'numeric',
+                            month: 'short',
+                            year: 'numeric'
+                        }).format(new Date(releaseDate))}
+                    </Style.Description>
+                </Style.Block>
+
+                <Style.Block>
+                    <Style.Label>Platforms</Style.Label>
+                    <Style.IconsWrapper>
+                        {platforms.map((icon: Platform) => (
+                            <Style.Icon key={icon}>{platformIcons[icon]}</Style.Icon>
+                        ))}
+                    </Style.IconsWrapper>
+                </Style.Block>
+
+                <Style.Block>
+                    <Style.Label>Publisher</Style.Label>
+                    <Style.Description>2K</Style.Description>
+                </Style.Block>
+
+                <Style.Block>
+                    <Style.Label>Rating</Style.Label>
+                    <Style.Description>{rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`}</Style.Description>
+                </Style.Block>
+
+                <Style.Block>
+                    <Style.Label>Genres</Style.Label>
+                    <Style.Description>{genres.join(' / ')}</Style.Description>
+                </Style.Block>
+            </Style.Content>
+        </Style.Wrapper>
+    );
+};
+
+export default GameDetails;
