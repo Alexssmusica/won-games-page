935d749747e50d063fe3adcbb3939857b599b08d
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-06-03 19:05:06 -0300

    Add TextContent

---
+++ src/components/TextContent/index.tsx
@@ -0,0 +1,20 @@
+import Heading from '../Heading';
+import * as Style from './styles';
+
+export type TextContentProps = {
+    title?: string;
+    content: string;
+};
+
+const TextContent = ({ title, content }: TextContentProps) => (
+    <Style.Wrapper>
+        {!!title && (
+            <Heading lineLeft lineColor="secondary">
+                {title}
+            </Heading>
+        )}
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+    </Style.Wrapper>
+);
+
+export default TextContent;
