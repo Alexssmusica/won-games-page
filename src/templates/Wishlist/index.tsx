1f0360e35c720d1d008ff50c94c932b0d83f0fa4
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-06-04 16:04:16 -0300

    Add Wishlist initial tests

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,9 +1,28 @@
-import * as Style from './styles';
+import Base from 'templates/Base';
 
-const Wishlist = () => (
-    <Style.Wrapper>
-        <h1>Wishlist</h1>
-    </Style.Wrapper>
+import Heading from 'components/Heading';
+import Showcase from 'components/Showcase';
+import { Container } from 'components/Container';
+import { GameCardProps } from 'components/GameCard';
+import { HighlightProps } from 'components/Highlight';
+
+// import * as Style from './styles';
+
+export type WishlistTemplateProps = {
+    recommendedGames: GameCardProps[];
+    recommendedHighlight: HighlightProps;
+};
+
+const Wishlist = ({ recommendedGames, recommendedHighlight }: WishlistTemplateProps) => (
+    <Base>
+        <Container>
+            <Heading lineLeft lineColor="secondary">
+                Wishlist
+            </Heading>
+        </Container>
+
+        <Showcase title="You may like these games" games={recommendedGames} highlight={recommendedHighlight} />
+    </Base>
 );
 
 export default Wishlist;
