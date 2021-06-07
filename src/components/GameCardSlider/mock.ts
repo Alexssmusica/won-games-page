c5dac5de9433c93c6c18c06d5f61dd373691d1c8
Alex Santos de Souza <alexssmusica@gmail.com>
2021-06-07 13:53:24 -0300

    ajustes nome jogos do mock

--- src/components/GameCardSlider/mock.ts
+++ src/components/GameCardSlider/mock.ts
@@ -1,34 +1,34 @@
 export default [
     {
-        title: 'Population Zero',
+        title: 'Zork',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x140',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'Space Invaders',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x141',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'Frogger',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x142',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'Joust',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x143',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'Pac-Man',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x144',
         price: 'R$ 235,00',
--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -6,28 +6,28 @@ import GameCardSlider from '.';
 
 const items = [
     {
-        title: 'Population Zero',
+        title: 'Pac-Man',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x140',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'Mike Tysons Punch-Out!!',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x141',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'R-Type',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x142',
         price: 'R$ 235,00',
         promotionalPrice: 'R$ 215,00'
     },
     {
-        title: 'Population Zero',
+        title: 'Wolfenstein 3D',
         developer: 'Rockstar Games',
         img: 'https://source.unsplash.com/user/willianjusten/300x143',
         price: 'R$ 235,00',
