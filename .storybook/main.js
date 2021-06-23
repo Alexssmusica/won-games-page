7824bc642a47217a2c2d3e60fb3e8e732632491a
Alex Santos de Souza <alexssmusica@gmail.com>
2021-06-23 17:17:41 -0300

    update libs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,6 @@
 module.exports = {
-  typescript: {
-    reactDocgen: 'react-docgen',
-  },
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials',
-     '@storybook/addon-postcss',
-     {
-       name: '@storybook/addon-postcss',
-       options: {
-         postcssLoaderOptions: {
-           implementation: require('postcss'),
-         },
-       },
-    }],
+  addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
