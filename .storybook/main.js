8a3aeb4a8d996b018d89953c208669ac18cad927
Alex Santos de Souza <alexssmusica@gmail.com>
2021-07-02 10:14:32 -0300

    feat: storybook build webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
-  }
+  },
+  features: {
+    postcss: false
+  },
+  core: {
+    builder: 'webpack5'
+  },
 }
