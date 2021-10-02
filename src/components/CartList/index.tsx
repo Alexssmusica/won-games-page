84f43d71e6821eebb3ca0ab7631d32aebe5acf05
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-10-02 18:39:01 -0300

    Update CartList to have hasButton option

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
 import GameItem, { GameItemProps } from '../GameItem';
+import Button from '../Button';
 
 import * as Style from './styles';
 
 export type CartListProps = {
     items: GameItemProps[];
     total: string;
+    hasButton?: boolean;
 };
 
-const CartList = ({ items, total }: CartListProps) => (
+const CartList = ({ items, total, hasButton = false }: CartListProps) => (
     <Style.Wrapper>
         {items.map((item) => (
             <GameItem key={item.title} {...item} />
         ))}
 
         <Style.Footer>
-            Total <Style.Total>{total}</Style.Total>
+            {!hasButton && <span>Total:</span>}
+            <Style.Total>{total}</Style.Total>
+            {hasButton && (
+                <Link href="/cart">
+                    <Button as="a">Buy it now</Button>
+                </Link>
+            )}
         </Style.Footer>
     </Style.Wrapper>
 );
