27548925c6832783d00c88e394e4d28ce76d99d1
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-10-02 23:47:16 -0300

    ajutes ano footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,67 +4,71 @@ import Logo from 'components/Logo';
 import Heading from 'components/Heading';
 import * as Style from './styles';
 
-const Footer = () => (
-    <Style.Wrapper>
-        <Logo color="black" />
-        <Style.Content>
-            <Style.Column>
-                <Heading color="black" size="small" lineBottom lineColor="secondary">
-                    Contact Us
-                </Heading>
+const Footer = () => {
+    const CurrentYear = new Date().getFullYear();
 
-                <a href="mailto:sac@wongames.com">sac@wongames.com</a>
-            </Style.Column>
-            <Style.Column>
-                <Heading color="black" lineColor="secondary" lineBottom size="small">
-                    Follow us
-                </Heading>
+    return (
+        <Style.Wrapper>
+            <Logo color="black" />
+            <Style.Content>
+                <Style.Column>
+                    <Heading color="black" size="small" lineBottom lineColor="secondary">
+                        Contact Us
+                    </Heading>
 
-                <nav aria-labelledby="social media">
-                    <a href="https://www.instagram.com/won-games" target="_blank" rel="noopenner, noreferrer">
-                        Instagram
-                    </a>
-                    <a href="https://www.twitter.com/won-games" target="_blank" rel="noopenner, noreferrer">
-                        Twitter
-                    </a>
-                    <a href="https://www.youtube.com/won-games" target="_blank" rel="noopenner, noreferrer">
-                        Youtube
-                    </a>
-                    <a href="https://www.facebook.com/won-games" target="_blank" rel="noopenner, noreferrer">
-                        Facebook
-                    </a>
-                </nav>
-            </Style.Column>
+                    <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+                </Style.Column>
+                <Style.Column>
+                    <Heading color="black" lineColor="secondary" lineBottom size="small">
+                        Follow us
+                    </Heading>
 
-            <Style.Column>
-                <Heading color="black" lineColor="secondary" lineBottom size="small">
-                    Links
-                </Heading>
+                    <nav aria-labelledby="social media">
+                        <a href="https://www.instagram.com/won-games" target="_blank" rel="noopenner, noreferrer">
+                            Instagram
+                        </a>
+                        <a href="https://www.twitter.com/won-games" target="_blank" rel="noopenner, noreferrer">
+                            Twitter
+                        </a>
+                        <a href="https://www.youtube.com/won-games" target="_blank" rel="noopenner, noreferrer">
+                            Youtube
+                        </a>
+                        <a href="https://www.facebook.com/won-games" target="_blank" rel="noopenner, noreferrer">
+                            Facebook
+                        </a>
+                    </nav>
+                </Style.Column>
 
-                <nav aria-labelledby="footer resources">
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>
-                    <Link href="/games">
-                        <a>Store</a>
-                    </Link>
-                    <Link href="/search">
-                        <a>Buscar</a>
-                    </Link>
-                </nav>
-            </Style.Column>
+                <Style.Column>
+                    <Heading color="black" lineColor="secondary" lineBottom size="small">
+                        Links
+                    </Heading>
 
-            <Style.Column aria-labelledby="footer-contact">
-                <Heading color="black" lineColor="secondary" lineBottom size="small">
-                    Location
-                </Heading>
-                <span>Lorem ipsum dolor sit.</span>
-                <span>Lorem Ipsum</span>
-                <span>Lorem, ipsum dolor.</span>
-            </Style.Column>
-        </Style.Content>
-        <Style.Copyright>Won Games 2020 © All rights reserved.</Style.Copyright>
-    </Style.Wrapper>
-);
+                    <nav aria-labelledby="footer resources">
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                        <Link href="/games">
+                            <a>Store</a>
+                        </Link>
+                        <Link href="/search">
+                            <a>Buscar</a>
+                        </Link>
+                    </nav>
+                </Style.Column>
+
+                <Style.Column aria-labelledby="footer-contact">
+                    <Heading color="black" lineColor="secondary" lineBottom size="small">
+                        Location
+                    </Heading>
+                    <span>Lorem ipsum dolor sit.</span>
+                    <span>Lorem Ipsum</span>
+                    <span>Lorem, ipsum dolor.</span>
+                </Style.Column>
+            </Style.Content>
+            <Style.Copyright>Won Games {CurrentYear} © All rights reserved.</Style.Copyright>
+        </Style.Wrapper>
+    );
+};
 
 export default Footer;
