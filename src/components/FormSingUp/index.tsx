2e4be9bdc7f37cbffbad955d860f2f1e1b761ca4
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-12-23 16:52:48 -0300

    Add useMutation to FormSignUp and change TextField to use onInputChange

--- src/components/FormSingUp/index.tsx
+++ src/components/FormSingUp/index.tsx
@@ -1,30 +1,88 @@
 import Link from 'next/link';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import { Email, Lock, AccountCircle } from '@styled-icons/material-outlined';
 
+import { MUTATION_REGISTER } from 'graphql/mutation/register';
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes';
 import { FormWrapper, FormLink } from 'components/Form';
 import TextField from 'components/TextField';
 import Button from 'components/Button';
 
-const FormSingUp = () => (
-	<FormWrapper>
-		<form>
-			<TextField name="name" placeholder="Name" type="text" icon={<AccountCircle />} />
-			<TextField name="email" placeholder="Email" type="email" icon={<Email />} />
-			<TextField name="password" placeholder="Password" type="password" icon={<Lock />} />
-			<TextField name="confirm-password" placeholder="Confirm password" type="password" icon={<Lock />} />
+const FormSingUp = () => {
+	const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+		email: '',
+		username: '',
+		password: ''
+	});
 
-			<Button size="large" fullWidth>
-				Sign up now
-			</Button>
+	const [createUser] = useMutation(MUTATION_REGISTER);
 
-			<FormLink>
-				Already have an account?{' '}
-				<Link href="/sign-in">
-					<a>Sign in</a>
-				</Link>
-			</FormLink>
-		</form>
-	</FormWrapper>
-);
+	const handleInput = (field: string, value: string) => {
+		setValues((state) => ({
+			...state,
+			[field]: value
+		}));
+	};
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		await createUser({
+			variables: {
+				input: {
+					email: values.email,
+					username: values.username,
+					password: values.password
+				}
+			}
+		});
+	};
+
+	return (
+		<FormWrapper>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					name="username"
+					placeholder="Username"
+					type="text"
+					onInputChange={(value) => handleInput('username', value)}
+					icon={<AccountCircle />}
+				/>
+				<TextField
+					name="email"
+					placeholder="Email"
+					type="email"
+					onInputChange={(value) => handleInput('email', value)}
+					icon={<Email />}
+				/>
+				<TextField
+					name="password"
+					placeholder="Password"
+					type="password"
+					onInputChange={(value) => handleInput('password', value)}
+					icon={<Lock />}
+				/>
+				<TextField
+					name="confirm-password"
+					placeholder="Confirm password"
+					onInputChange={(value) => handleInput('confirm-password', value)}
+					type="password"
+					icon={<Lock />}
+				/>
+
+				<Button type="submit" size="large" fullWidth>
+					Sign up now
+				</Button>
+
+				<FormLink>
+					Already have an account?{' '}
+					<Link href="/sign-in">
+						<a>Sign in</a>
+					</Link>
+				</FormLink>
+			</form>
+		</FormWrapper>
+	);
+};
 
 export default FormSingUp;
