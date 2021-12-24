a75a1859513582aea6fcfe219fc871a8fff27203
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-12-24 16:31:07 -0300

    Add protected routes

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -5,18 +5,22 @@ import { initializeApollo } from 'utils/apollo';
 import { GetRecommended } from 'graphql/generated/GetRecommended';
 import { gamesMapper, highlightMapper } from 'utils/mappers';
 import { GET_RECOMMENDED } from 'graphql/queries/recommended';
+import protectedRoutes from 'utils/protected-routes';
+import { GetServerSidePropsContext } from 'next';
 
 export default function WishlistPage(props: WishlistTemplateProps) {
 	return <Wishlist {...props} />;
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	const session = await protectedRoutes(context);
 	const apolloClient = initializeApollo();
 
 	const { data } = await apolloClient.query<GetRecommended>({ query: GET_RECOMMENDED });
 
 	return {
 		props: {
+			session,
 			games: gamesMock,
 			recommendedGames: gamesMapper(data.recommended?.section?.games),
 			recommendedHighlight: highlightMapper(data.recommended?.section?.highlight),
