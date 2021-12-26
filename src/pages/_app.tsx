1e5a2574d056fb43dcc4f10676c1aa80d00ff0d2
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-12-26 20:32:45 -0300

    Add initial structure to useWishlist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { CartProvider } from 'hooks/use-cart';
+import { WishlistProvider } from 'hooks/use-wishlist';
 
 import { useApollo } from 'hooks/use-apollo';
 import GlobalStyles from 'styles/global';
@@ -17,16 +18,18 @@ function App({ Component, pageProps }: AppProps) {
 			<ApolloProvider client={client}>
 				<ThemeProvider theme={theme}>
 					<CartProvider>
-						<Head>
-							<title>Won Games</title>
-							<link rel="shortcut icon" href="/img/icon-512.png" />
-							<link rel="apple-touch-icon" href="/img/icon-512.png" />
-							<link rel="manifest" href="/manifest.json" />
-							<meta name="description" content="The best Game Stores in the world!" />
-						</Head>
-						<GlobalStyles />
-						<NextNProgress color="#F231A5" startPosition={0.3} stopDelayMs={200} height={5} />
-						<Component {...pageProps} />
+						<WishlistProvider>
+							<Head>
+								<title>Won Games</title>
+								<link rel="shortcut icon" href="/img/icon-512.png" />
+								<link rel="apple-touch-icon" href="/img/icon-512.png" />
+								<link rel="manifest" href="/manifest.json" />
+								<meta name="description" content="The best Game Stores in the world!" />
+							</Head>
+							<GlobalStyles />
+							<NextNProgress color="#F231A5" startPosition={0.3} stopDelayMs={200} height={5} />
+							<Component {...pageProps} />
+						</WishlistProvider>
 					</CartProvider>
 				</ThemeProvider>
 			</ApolloProvider>
