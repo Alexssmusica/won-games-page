e4f3af9594f37981a295b1fc93865f45b32cac9c
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-12-26 22:37:21 -0300

    Add WishlistProvider to custom render test

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,21 +1,29 @@
 import { ReactElement } from 'react';
-import { render, RenderOptions } from '@testing-library/react';
-import { CartContext, CartContextData, CartContextDefaultValues } from 'hooks/use-cart';
-
 import { ThemeProvider } from 'styled-components';
+import { render, RenderOptions } from '@testing-library/react';
+
+import { CartContext, CartContextData, CartContextDefaultValues } from 'hooks/use-cart';
+import { WishlistContext, WishlistContextData, WishlistContextDefaultValues } from 'hooks/use-wishlist';
 import theme from 'styles/theme';
 
 type CustomRenderProps = {
 	cartProviderProps?: CartContextData;
+	wishlistProviderProps?: WishlistContextData;
 } & Omit<RenderOptions, 'queries'>;
 
 const customRender = (
 	ui: ReactElement,
-	{ cartProviderProps = CartContextDefaultValues, ...renderOptions }: CustomRenderProps = {}
+	{
+		cartProviderProps = CartContextDefaultValues,
+		wishlistProviderProps = WishlistContextDefaultValues,
+		...renderOptions
+	}: CustomRenderProps = {}
 ) =>
 	render(
 		<ThemeProvider theme={theme}>
-			<CartContext.Provider value={cartProviderProps}>{ui}</CartContext.Provider>
+			<CartContext.Provider value={cartProviderProps}>
+				<WishlistContext.Provider value={wishlistProviderProps}>{ui}</WishlistContext.Provider>
+			</CartContext.Provider>
 		</ThemeProvider>,
 		renderOptions
 	);
