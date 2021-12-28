dd692fe0ee957800570127381084a26cc4746e7b
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-12-28 18:43:14 -0300

    Add remove and add to wishlistButton

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -12,6 +12,10 @@ const WishlistButton = ({ id, hasText, size = 'small' }: WishlistButtonProps) =>
 	const [session] = useSession();
 	const { isInWishlist, addToWishlist, removeFromWishlist } = useWishlist();
 
+	const handleClick = () => {
+		isInWishlist(id) ? removeFromWishlist(id) : addToWishlist(id);
+	};
+
 	const ButtonText = isInWishlist(id) ? 'Remove from Wishlist' : 'Add to Wishlist';
 
 	if (!session) return null;
@@ -21,6 +25,7 @@ const WishlistButton = ({ id, hasText, size = 'small' }: WishlistButtonProps) =>
 			icon={isInWishlist(id) ? <Favorite aria-label={ButtonText} /> : <FavoriteBorder aria-label={ButtonText} />}
 			minimal
 			size={size}
+			onClick={handleClick}
 		>
 			{hasText && ButtonText}
 		</Button>
